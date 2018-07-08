b5afc8251574a59ca211cde76c4f47b97d683637
gchlebus <gchlebus@gmail.com>
2018-07-08 23:38:23 +0200

    Add test for source file change.

--- test_automake.py
+++ test_automake.py
@@ -22,27 +22,44 @@ def changedir(dirpath):
     os.chdir(cwd)
 
 @contextlib.contextmanager
-def cleanupfile(filename):
+def cleanupfiles(*filenames):
   try:
     yield
   finally:
-    if os.path.exists(filename):
-      os.remove(filename)
+    for f in filenames:
+      if os.path.exists(f):
+        os.remove(f)
+
 
 def test_simplerun():
   automake.STOP_AFTER_NSEC = 1
-  with changedir(TEST_MAKEFILES_DIR), cleanupfile(TMP_FILENAME):
+  with changedir(TEST_MAKEFILES_DIR), cleanupfiles(TMP_FILENAME):
     automake.automake(test_simplerun.__name__, debug=True)
     assert os.path.exists(TMP_FILENAME)
 
 def test_makefilechange():
   automake.STOP_AFTER_NSEC = 2
-  with changedir(TEST_MAKEFILES_DIR), cleanupfile(TMP_FILENAME):
+  with changedir(TEST_MAKEFILES_DIR), cleanupfiles(TMP_FILENAME):
     t = Thread(target=automake.automake, args=(test_makefilechange.__name__,))
     t.start()
     time.sleep(1)
-    open(test_makefilechange.__name__, 'a').close()
-    os.utime(test_makefilechange.__name__, None)
+    touch_file(test_makefilechange.__name__)
     t.join()
     with open(TMP_FILENAME, 'r') as f:
       assert 2 == len(f.readlines())
+
+def test_sourcefilechange():
+  automake.STOP_AFTER_NSEC = 2
+  tmp_source_file = 'source_file'
+  with changedir(TEST_MAKEFILES_DIR), cleanupfiles(TMP_FILENAME, tmp_source_file):
+    open(tmp_source_file, 'w').close()
+    t = Thread(target=automake.automake, args=(test_sourcefilechange.__name__,))
+    t.start()
+    time.sleep(1)
+    touch_file(tmp_source_file)
+    t.join()
+    with open(TMP_FILENAME, 'r') as f:
+      assert 2 == len(f.readlines())
+
+def touch_file(filename):
+  os.utime(filename, None)
